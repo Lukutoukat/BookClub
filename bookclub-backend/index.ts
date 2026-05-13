2836662c7968fb8b168d36a4c34ed549a37126c7
Nea Kovalainen <neakovalaine@gmail.com>
2026-05-13 15:22:14 +0300

    connected front and back

--- bookclub-backend/index.ts
+++ bookclub-backend/index.ts
@@ -1,10 +1,20 @@
 import express from 'express';
 const app = express();
 
+app.use(express.static('dist'))
+
 app.get('/ping', (_req, res) => {
   res.send('pong');
 });
 
+app.get('/', (_req, res) => {
+  res.send('<h1>Hello World!<h1>')
+})
+
+app.get('/api/books', (_req, res) => {
+  res.send(['book1', 'book2', 'book3'])
+  console.log('smth happened in backend')
+})
 const PORT = 3003;
 
 app.listen(PORT, () => {
--- bookclub-front/src/App.tsx
+++ bookclub-front/src/App.tsx
@@ -1,7 +1,26 @@
-const App = () => (
-  <div>
-    <p>Hello world</p>
-  </div>
-)
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const baseUrl = '/api/books'
+const App = () => {
+  const [books, setBooks] = useState([])
+
+  useEffect(() => {
+    axios
+      .get(baseUrl)
+      .then(response => {
+        console.log(books)
+        setBooks(response.data)
+    })
+  }, [])
+  return (
+    <div>
+      <h1>books</h1>
+      <ul>
+        {books.map(book => <li>{book}</li>)}
+      </ul>
+    </div>
+  )
+}
 
 export default App
