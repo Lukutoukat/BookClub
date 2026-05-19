2aa784c35c30f85d0106c8d5ce9c674941bde662
MatiasSlotboom <matias.slotboom@helsinki.fi>
2026-05-19 12:42:26 +0300

    Set up tailwind css and shadcn

--- bookclub-front/vite.config.ts
+++ bookclub-front/vite.config.ts
@@ -1,15 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-    server: {
+  plugins: [
+    react(),
+    tailwindcss()
+  ],
+  server: {
     proxy: {
       '/api': {
         target: 'http://bookclub-backend:3003',
         changeOrigin: true,
       },
     }
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
 })
