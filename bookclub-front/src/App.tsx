44447485745beb41aee3f8ab88226ba726be111e
Nea Kovalainen <neakovalaine@gmail.com>
2026-05-19 13:48:26 +0300

    created component folder and movef bookform there

--- bookclub-front/src/App.tsx
+++ bookclub-front/src/App.tsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react'
 import bookService from './services/books'
 import type { Book, CreateBook } from  './services/books'
+import BookForm from './components/BookForm'
+
 
 const App = () => {
   const [books, setBooks] = useState<Book[]>([])
@@ -71,73 +73,11 @@ const App = () => {
           </li>
         ))}
       </ul>
-      <form onSubmit={addBook}>
-        <div>
-          ISBN:
-          <input
-            name="isbn"
-            value={newBook.isbn}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          Name:
-          <input
-            name="name"
-            value={newBook.name}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          Author:
-          <input
-            name="author"
-            value={newBook.author}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          Year:
-          <input
-            name="year"
-            value={newBook.year}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          Pages:
-          <input
-            name="pages"
-            value={newBook.pages}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          Language:
-          <input
-            name="language"
-            value={newBook.language}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          Genre:
-          <input
-            name="genre"
-            value={newBook.genre}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          Comment:
-          <textarea
-            name="comment"
-            value={newBook.comment}
-            onChange={handleChange}
-          />
-        </div>
-        <button type="submit">save</button>
-      </form>
+      <BookForm 
+      addBook={(event: React.SyntheticEvent<HTMLFormElement>) => addBook(event)} 
+      newBook={newBook}
+      handleChange={(event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => handleChange(event)}
+      />
     </div>
   )
 }
