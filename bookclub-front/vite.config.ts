fa4207a22c66681cd8adfcdce482c3ca93170583
Eemil Aarnio <aarnioem@gmail.com>
2026-05-20 10:51:09 +0300

    Docker hot reload for dev environment
    Co-authored-by: saloaaro <165225065+salooaro@users.noreply.github.com>

--- bookclub-front/vite.config.ts
+++ bookclub-front/vite.config.ts
@@ -4,7 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-    server: {
+  server: {
+    host: '0.0.0.0',
+    hmr: {
+      host: 'localhost',
+      port: 13000,
+    },
     proxy: {
       '/api': {
         target: 'http://bookclub-backend:3003',
