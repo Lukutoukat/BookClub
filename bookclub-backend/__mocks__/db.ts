4f5faf31fad65d4859fb6bf6d295720043763958
Eemil Aarnio <aarnioem@gmail.com>
2026-05-22 13:23:10 +0300

    jest setup and register test

---
+++ bookclub-backend/__mocks__/db.ts
@@ -0,0 +1,16 @@
+import { jest } from '@jest/globals'
+
+// Mock Prisma client
+export const prisma = {
+  user: {
+    findFirst: jest.fn(),
+    create: jest.fn(),
+    findMany: jest.fn(),
+    delete: jest.fn(),
+  },
+  book: {
+    findMany: jest.fn(),
+    create: jest.fn(),
+    delete: jest.fn(),
+  },
+}
