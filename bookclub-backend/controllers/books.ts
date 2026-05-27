4292cae51ec6beab84c10f34a739e51f70ea7714
HerttaVierikko <hertta.vierikko@gmail.com>
2026-05-27 09:12:31 +0300

    Add user authentication for adding books

--- bookclub-backend/controllers/books.ts
+++ bookclub-backend/controllers/books.ts
@@ -1,5 +1,6 @@
 import express, { type Request, type Response } from 'express'
 import { prisma } from '../db.ts'
+import jwt from 'jsonwebtoken'
 
 const bookRouter = express.Router()
 
@@ -12,6 +13,14 @@ interface Book {
   comment: string,
   language: string,
   genre: string
+}
+
+const getTokenFrom = (request: Request<unknown, unknown, Book>): string | null => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
 }
 
 bookRouter.get('/', async (_req: Request, res: Response) => {
@@ -27,6 +36,36 @@ bookRouter.get('/', async (_req: Request, res: Response) => {
 bookRouter.post('/', async (req: Request<unknown, unknown, Book>, res: Response) => {
   const newBook: Book = req.body
 
+  const token = getTokenFrom(req)
+    if (!token) {
+      return res.status(401).json({
+        error: 'missin token'
+      })
+    }
+
+  const decodedToken = jwt.verify(
+    token,
+    process.env.SECRET as string
+  ) as { id: number }
+
+  if (!decodedToken.id) {
+    return res.status(401).json({
+      error: 'token invalid'
+    })
+  }
+
+  const user = await prisma.user.findUnique({
+    where: {
+      id: decodedToken.id
+    }
+  })
+
+  if (!user) {
+    return res.status(400).json({
+      error: 'userId missing or not valid'
+    })
+  }
+
   try {
     await prisma.book.create({
       data: {
@@ -40,10 +79,10 @@ bookRouter.post('/', async (req: Request<unknown, unknown, Book>, res: Response)
         genre: newBook.genre,
       }
     })
-    res.json(newBook)
+    return res.json(newBook)
   } catch (error) {
     console.error('POST /api/books error:', error)
-    res.status(500).json({ error: 'database error' })
+    return res.status(500).json({ error: 'database error' })
   }
 })
 
