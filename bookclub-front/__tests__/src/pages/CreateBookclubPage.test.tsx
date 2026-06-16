4e3368f480ea2ae2761aad40349e417ab0f36821
Jaana Haussalo <145219384+jaahauss@users.noreply.github.com>
2026-06-16 13:20:47 +0300

    Added frontend tests for creating a bookclub

---
+++ bookclub-front/__tests__/src/pages/CreateBookclubPage.test.tsx
@@ -0,0 +1,35 @@
+import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import { test, expect, describe } from "vitest";
+import CreateBookclubPage from "@/pages/CreateBookclubPage";
+
+const renderWithRouter = (component: React.ReactElement) => {
+  return render(<BrowserRouter>{component}</BrowserRouter>);
+};
+
+describe("CreateBookclubPage", () => {
+
+  test("renders page header content", () => {
+    renderWithRouter(<CreateBookclubPage />);
+
+    expect(screen.getByText("New book club")).toBeDefined();
+    expect(
+      screen.getByText(
+        "Create a new book club for you and your friends to enjoy reading together."
+      )
+    ).toBeDefined();
+  });
+
+  test("renders bookclub form", () => {
+    renderWithRouter(<CreateBookclubPage />);
+
+    expect(
+      screen.getByRole("button", { name: "Create" })
+    ).toBeDefined();
+
+    expect(
+      screen.getByLabelText("Name your book club")
+    ).toBeDefined();
+  });
+
+});
