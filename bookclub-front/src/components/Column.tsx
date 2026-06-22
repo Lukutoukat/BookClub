8ecee0e2bd9738df1c73187f59017f9cdf90166d
MatiasSlotboom <matias.slotboom@helsinki.fi>
2026-06-22 15:06:08 +0300

    Column applies break-inside-avoid to all children

--- bookclub-front/src/components/Column.tsx
+++ bookclub-front/src/components/Column.tsx
@@ -5,7 +5,7 @@ export const Column = ({ children }: { children: React.ReactNode }) => {
 			[column-fill:_balance]
 			columns-1
 			2xl:columns-2        /* Extra Large (1536px+): 5 cards per row */
-			2xl:break-inside-avoid 2xl:gap-8 2xl:inline-block
+			[&>*]:break-inside-avoid 2xl:gap-8
 			[&>*]:mb-4 2xl:[&>*]:mb-8
 			"
 		>
