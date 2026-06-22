79548ff463eb7f9cc7e7ea509597d665948f943f
MatiasSlotboom <matias.slotboom@helsinki.fi>
2026-06-22 10:32:23 +0300

    Adjust bookspage test for default emptymessage
    Co-authored-by: saloaaro <165225065+saloaaro@users.noreply.github.com>

--- bookclub-front/__tests__/src/pages/BooksPage.test.tsx
+++ bookclub-front/__tests__/src/pages/BooksPage.test.tsx
@@ -118,7 +118,7 @@ describe('BooksPage', () => {
 			renderWithRouter(<BooksPage />)
 
 			await waitFor(() => {
-				expect(screen.getByText('No books suggested yet. Be the first to add one!')).toBeDefined()
+				expect(screen.getByText('No books yet.')).toBeDefined()
 			})
 		})
 
