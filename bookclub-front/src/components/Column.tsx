759b23777d3b45d98b84d9282a87ddaaff864434
MatiasSlotboom <matias.slotboom@helsinki.fi>
2026-06-22 14:56:43 +0300

    More responsive padding

--- bookclub-front/src/components/Column.tsx
+++ bookclub-front/src/components/Column.tsx
@@ -3,13 +3,10 @@ export const Column = ({ children }: { children: React.ReactNode }) => {
 		<div
 			className="
 			[column-fill:_balance]
-			columns-1            /* Mobile (under 768px wide): 1 card per row */
-			md:columns-1         /* Tablet (768px - 1023px): 2 cards per row */
-			lg:columns-1         /* Laptop (1024px - 1279px): 3 cards per row */
-			xl:columns-1         /* Desktop (1280px - 1535px): 4 cards per row */
+			columns-1
 			2xl:columns-2        /* Extra Large (1536px+): 5 cards per row */
-			break-inside-avoid gap-8 inline-block
-			[&>*]:mb-8
+			2xl:break-inside-avoid 2xl:gap-8 2xl:inline-block
+			[&>*]:mb-4 2xl:[&>*]:mb-8
 			"
 		>
 			{children}
