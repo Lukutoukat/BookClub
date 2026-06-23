9a7cb94f3f9a3f38e4b563fbe49b27ec593110e9
MatiasSlotboom <matias.slotboom@helsinki.fi>
2026-06-23 11:26:38 +0300

    Create 3xl breakpoint which activates 3 columns on bigger displays

--- bookclub-front/src/components/Column.tsx
+++ bookclub-front/src/components/Column.tsx
@@ -4,7 +4,8 @@ export const Column = ({ children }: { children: React.ReactNode }) => {
 			className="
 			[column-fill:_balance]
 			columns-1
-			2xl:columns-2        /* Extra Large (1536px+): 5 cards per row */
+			2xl:columns-2
+			3xl:columns-3        /* Extra Large (1536px+): 5 cards per row */
 			[&>*]:break-inside-avoid 2xl:gap-8
 			[&>*]:mb-4 2xl:[&>*]:mb-8
 			"
