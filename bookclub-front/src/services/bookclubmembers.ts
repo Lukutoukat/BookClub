6c2032eead421de1b08ba6c406185b4dfe3722ad
Wirebin <107724483+Wirebin@users.noreply.github.com>
2026-09-05 20:56:32 +0300

    feat: add club member removal feature for admin members

--- bookclub-front/src/services/bookclubmembers.ts
+++ bookclub-front/src/services/bookclubmembers.ts
@@ -5,9 +5,15 @@ const baseUrl = '/api/bookclubmembers'
 
 export interface BookclubMember {
 	id: string
+	user_id: string
 	user_role: number
 	invite_code: string
 	bookclub_id: string
+	User?: {
+		id: string
+		name: string
+		email: string
+	}
 }
 
 export type AddBookClubMember = Omit<BookclubMember, 'id' | 'bookclub_id'>
@@ -22,7 +28,17 @@ const get = () => {
 	return axios.get<BookclubMember[]>(baseUrl, getAuthConfig()).then((res) => res.data)
 }
 
+const getByClubId = (club_id: string) => {
+	return axios.get<BookclubMember[]>(`${baseUrl}/${club_id}`, getAuthConfig()).then((res) => res.data)
+}
+
+const remove = (club_id: string, user_id: string) => {
+	return axios.delete<BookclubMember>(`${baseUrl}/${club_id}/${user_id}`, getAuthConfig()).then((res) => res.data)
+}
+
 export default {
 	create,
-	get
+	get,
+	getByClubId,
+	remove,
 }
