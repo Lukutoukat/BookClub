76e1f017c95179060f9501305841cbc8b2aeba62
Wirebin <107724483+Wirebin@users.noreply.github.com>
2026-09-22 15:45:49 +0300

    test: existing members are shown in BookclubPage

--- bookclub-front/__tests__/src/pages/BookclubPage.test.tsx
+++ bookclub-front/__tests__/src/pages/BookclubPage.test.tsx
@@ -75,4 +75,40 @@ describe('BookclubPage', () => {
 
 		expect(screen.queryByText('Suggest Book')).toBeNull()
 	})
+
+	it('renders correct members of a club', async () => {
+		const members = [
+			{
+				user_id: '1',
+				user_role: 1,
+				bookclub_id: '1',
+				User: {
+					id: '1',
+					name: 'Pekka'
+				}
+			},
+			{
+				user_id: '2',
+				user_role: 0,
+				bookclub_id: '1',
+				User: {
+					id: '2',
+					name: 'Liisa'
+				}
+			}
+		]
+
+		mockUseParams.mockReturnValue({ bookclubId: 'A' })
+
+		vi.mocked(bookclubmembersService.getByClubId).mockResolvedValue(members)
+
+		render(<BookclubPage />)
+
+		await waitFor(() => {
+			expect(screen.getByText('Club Members')).toBeDefined()
+			expect(screen.getByText('Pekka')).toBeDefined()
+			expect(screen.getByText('Liisa')).toBeDefined()
+			expect(screen.queryByText('Toni')).toBeNull()
+		})
+	})
 })
